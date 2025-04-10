111d3ba3c528400c1a7f28b068eea880cb14e17c
modeco80 <lily.modeco80@protonmail.ch>
2025-04-10 04:55:26 -0400

    some other tinkering

--- src/core/game/ssx3/game_api.h
+++ src/core/game/ssx3/game_api.h
@@ -1,6 +1,8 @@
 #ifndef GAME_API_H
 #define GAME_API_H 
 
+#include <ml/types.h>
+
 extern "C" {
         void cGame_load(void* pGame);
         void cGame_purge(void* pGame);
@@ -9,6 +11,8 @@ extern "C" {
 
         void* memset(void* ptr, int c, unsigned int length);
         void* memcpy(void* pDest, const void* pSrc, unsigned int length);
+
+        void scePrintf(const char* pszFormat, ...);
 
         void* memAlloc(unsigned int length, const char* name, unsigned flags = 0, unsigned flags2 = 0);
         void memFree(void* ptr);
@@ -32,6 +36,25 @@ extern "C" {
 
         unsigned int BXFILE_size(const char* pszFileName);
         unsigned int FILE_loadat(const char* pszFileName, void* pData, unsigned int maxsize); // returns amount of bytes actually loaded?
+
+        /* REALFILE */
+        struct FILESYSOPTS {
+                int size;
+                void*(*malloc)(u32 size);
+                void(*free)(void* ptr);
+                int DiscType;
+                int MaxPathLength;
+                bool IsDiscBootable;
+                char* DemoCdRoot;
+                char* REAL_filedevice;
+                u32 whoever_the_hell_knows_what;
+        };
+
+        void FILESYS_getopts(FILESYSOPTS* outOpts);
+        void FILESYS_setopts(FILESYSOPTS* inopts);
+
+        /* REALFILE async (aka hell) */
+        void ASYNCFILE_setcurrentpath(const char* pszPath);
 }
 
 #endif
--- src/core/game/ssx3/init.cpp
+++ src/core/game/ssx3/init.cpp
@@ -16,4 +16,17 @@ void cGame_renderHook(void* pGame) {
 void mlGameInit() {
 	// As a test, vtable hook cGame::render() :3
 	*((void**)0x0047d14c) = (void*)cGame_renderHook;
+
+	FILESYSOPTS opts;
+	FILESYS_getopts(&opts);
+
+	// dump the structure bro
+	scePrintf("Opts:\n");
+	scePrintf("Size: %d\n", opts.size);
+	scePrintf("DiscType: %d\n", opts.DiscType);
+	scePrintf("MaxPathLength: %d\n", opts.MaxPathLength);
+	scePrintf("IsDiscBootable: %s\n", opts.IsDiscBootable ? "yes" : "HELL no sir. hell no");
+	scePrintf("DemoCdRoot: %s\n", opts.DemoCdRoot);
+	scePrintf("REAL_filedevice: %s\n", opts.REAL_filedevice);
+	scePrintf("????????: %d (0x%08x)\n", opts.whoever_the_hell_knows_what, opts.whoever_the_hell_knows_what);
 }
