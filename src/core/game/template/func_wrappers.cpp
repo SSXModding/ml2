c4c3f8ec4e95e3bbb3ae0e9521c1a8a1cddd9175
modeco80 <lily.modeco80@protonmail.ch>
2025-04-11 01:27:17 -0400

    add mlStrCmp to the template func_wrappers.cpp

--- src/core/game/template/func_wrappers.cpp
+++ src/core/game/template/func_wrappers.cpp
@@ -1,6 +1,8 @@
 // This file provides hooks that the ml core uses
 // to provide certain C library functions to the mlcore.
 
+#include <ml/types.h>
+
 extern "C" {
 
 
@@ -18,6 +20,10 @@ void mlMemSet(void* pBuffer, int c, u32 len) {
 
 void mlMemCpy(void* pDestBuffer, const void* pSrcBuffer, u32 copyLen) {
 	return;
+}
+
+i32 mlStrCmp(const char *pszLeft, const char *pszRight) {
+	return 0;
 }
 
 void* mlLoadFile(const char* pszFileName) {
