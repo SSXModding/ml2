c280fd8695e0644c73e59669b7c3f5b3ca91949d
modeco80 <lily.modeco80@protonmail.ch>
2025-04-16 01:20:08 -0400

    Alter ssx3/us port to use the source filenames in its assembler products
    This makes a much cleaner build imo

--- src/core/game/ssx3/us/crt0_patch.asm
+++ src/core/game/ssx3/us/crt0_patch.asm
@@ -9,7 +9,7 @@ END_ADDRESS equ wordAlign(NEW_END_ADDRESS + 0x1000)
 ; This portion adjusts the _end address beyond its original limit
 ; (to hold the ML2 Core), by patching the EE crt0.s directly. 
 ; Is this nasty? Probably. Does it work? Uhhh fuck idk man ask Harold he will know
-.create "bin/ssx3/us/crt0_extend_heap.bin",0x00100180
+.create "bin/ssx3/us/crt0_patch.bin",0x00100180
 .org 0x00100180
     lui a0, hi(END_ADDRESS + 0x100)
     lui a1, 0
--- src/core/game/ssx3/us/libkernl_patch.asm
+++ src/core/game/ssx3/us/libkernl_patch.asm
@@ -6,7 +6,7 @@
 .expfunc wordAlign(addr), (addr + 3) & 0xfffffffc
 END_ADDRESS equ wordAlign(NEW_END_ADDRESS + 0x1000)
 
-.create "bin/ssx3/us/libkernl_extend_heap.bin",0x0045516c
+.create "bin/ssx3/us/libkernl_patch.bin",0x0045516c
 .org 0x0045516c
     dw END_ADDRESS
 .close
